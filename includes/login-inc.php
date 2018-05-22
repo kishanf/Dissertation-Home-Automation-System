916a5bc6251f42430fc5d2fa645f4563cc6e59d1
kishanf <KishanFokeer@Chiranjives-MacBook-Pro.local>
2018-05-22 10:57:26 +0100

    Final UI and LED controls
    Finished all styling for GUI and completed the RGB LED strip controls.
    Entire system now functions and works.

--- includes/login-inc.php
+++ includes/login-inc.php
@@ -33,7 +33,7 @@ if(isset($_POST['submit'])) {
                      $_SESSION['u_last'] = $row['user_last'];
                      $_SESSION['u_email'] = $row['user_email'];
                      $_SESSION['u_uid'] = $row['user_uid'];
-                     header("Location: ../panel.php?login=success");
+                     header("Location: ../home.php?login=success");
                      exit(); 
                  }
 
